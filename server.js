96c20daa4bb911d68ea487c4df3958f3c26cafb1
priyarizal <103598087+priyarizal@users.noreply.github.com>
2022-07-24 15:50:05 -0400

    post/get roites work

--- server.js
+++ server.js
@@ -1,45 +1,89 @@
-const express = require ("express");
-const fs = require ('fs');
+const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const termData = require('./db/db.json');
-const PORT = 3001;
+const uuid = require('./helpers/uuid');
+var noteData = require('./db/db.json');
+// const fs = require('fs');
 
+const PORT = process.env.PORT || 3002;
 const app = express();
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+
+//GET route for get started page
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET route for note taking pagr
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-console.log(notes)
-res.json(notes)
+    console.log(noteData);
+    res.json(noteData);
 
-console.info(`${req.method} request received to get notes`);
+    console.info(`${req.method} request received to get notes`);
 });
 
-app.get('/api/notes/:notes_id', (req, res) => {
-    if (req.body && req.params.notes_id) {
-      console.info(`${req.method} request received to get a single a notes`);
-      const notesId = req.params.notes_id;
-      for (let i = 0; i < reviews.length; i++) {
-        const currentReview = reviews[i];
-        if (currentReview.review_id === reviewId) {
-          res.json(currentReview);
-          return;
-        }
-      }
-      res.json('notes ID not found');
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`); 
+    var noteData;
+const { title, text } = req.body;
+
+if (title && text) {
+    const newNote = {
+        title,
+        text,
+        noteData_id: uuid(),
+
+    };
+
+fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+    } else {
+       let parsedNotes = JSON.parse(data)
+
+       parsedNotes.push(newNote);
+
+        noteData = parsedNotes;
+        
+
+       fs.writeFile(
+        './db/db.json',
+        JSON.stringify(noteData),
+        (writeErr) =>
+          writeErr
+            ? console.error(writeErr)
+            : console.info('Successfully updated notes!')
+       );
     }
-  });
+});
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
 
-    app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add a notes`);
+        console.log(response);
+        res.send(noteData);
+        } else {
+        res.json('Error in posting review');
+
+    }
+});
+
+// app.delete()
 
 
-    })
-
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
